798e7bcc93626c503429ec59a1ecbc8fd44b4c81
Marek Smogrovic <smogrovic.marek@gmail.com>
2024-05-18 13:41:25 +0200

    Add missing translation

--- frontend/src/components/UserUpdateForm.vue
+++ frontend/src/components/UserUpdateForm.vue
@@ -1,86 +1,82 @@
 <template>
-    <q-form ref="form" class="gap column" greedy>
-            <q-input
-            ref="mailRef"
-            v-model="user.email"
-            :label="t('auth.email')"
-            type="email"
-            :rules="[required(), isEmail()]"
-            lazy-rules
-            >
-                <template #prepend>
-                    <q-icon :name="mdiEmail" />
-                </template>
-            </q-input>
-            <q-input
-            v-model="user.first_name"
-            :label="t('users.first_name')"
-            type="text"
-            :rules="[required(), minLength(2), maxLength(50)]"
-            lazy-rules
-          >
-            <template #prepend>
-              <q-icon :name="mdiAccount" />
-            </template>
-          </q-input>
-          <q-input
-            v-model="user.last_name"
-            :label="t('users.last_name')"
-            type="text"
-            :rules="[required(), minLength(2), maxLength(80)]"
-            lazy-rules
-          >
-            <template #prepend>
-              <q-icon :name="mdiAccount" />
-            </template>
-          </q-input>
-            <q-input
-            ref="passwordRef"
-            v-model="user.password"
-            :label="t('auth.password')"
-            :type="isPwd ? 'password' : 'text'"
-            :rules="[ required(), minLength(6)]"
-          >
-            <template #prepend>
-              <q-icon :name="mdiLock" />
-            </template>
-            <template #append>
-              <q-icon
-                :name="isPwd ? mdiEyeOff : mdiEye"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
-            </template>
-          </q-input>
-            <q-select
-                :label="t('users.roles.role')"
-                v-model="user.role"
-                style="min-width: 200px"
-                class="col-12 col-sm-auto"
-                map-options
-                :rules="[required()]"
-                emit-value
-                :options="roles"
-                >
-                <template v-slot:selected-item="scope">
-                    <div class="row items-center">
-                    <q-icon size="1.25rem" color="grey-9" class="q-mr-sm" :name="scope.opt.icon" />
-                    <div>{{ scope.opt.label }}</div>
-                    </div>
-                </template>
+  <q-form ref="form" class="gap column container-dashboard q-pa-lg" greedy>
+    <q-input
+      ref="mailRef"
+      v-model="user.email"
+      :label="t('auth.email')"
+      type="email"
+      :rules="[required(), isEmail()]"
+      lazy-rules
+    >
+      <template #prepend>
+        <q-icon :name="mdiEmail" />
+      </template>
+    </q-input>
+    <q-input
+      v-model="user.first_name"
+      :label="t('users.first_name')"
+      type="text"
+      :rules="[required(), minLength(2), maxLength(50)]"
+      lazy-rules
+    >
+      <template #prepend>
+        <q-icon :name="mdiAccount" />
+      </template>
+    </q-input>
+    <q-input
+      v-model="user.last_name"
+      :label="t('users.last_name')"
+      type="text"
+      :rules="[required(), minLength(2), maxLength(80)]"
+      lazy-rules
+    >
+      <template #prepend>
+        <q-icon :name="mdiAccount" />
+      </template>
+    </q-input>
+    <q-input
+      ref="passwordRef"
+      v-model="user.password"
+      :label="t('auth.password')"
+      :type="isPwd ? 'password' : 'text'"
+      :rules="[required(), minLength(6)]"
+    >
+      <template #prepend>
+        <q-icon :name="mdiLock" />
+      </template>
+      <template #append>
+        <q-icon :name="isPwd ? mdiEyeOff : mdiEye" class="cursor-pointer" @click="isPwd = !isPwd" />
+      </template>
+    </q-input>
+    <q-select
+      :label="t('users.roles.role')"
+      v-model="user.role"
+      style="min-width: 200px"
+      class="col-12 col-sm-auto"
+      map-options
+      :rules="[required()]"
+      emit-value
+      :options="roles"
+    >
+      <template v-slot:selected-item="scope">
+        <div class="row items-center">
+          <q-icon size="1.25rem" color="grey-9" class="q-mr-sm" :name="scope.opt.icon" />
+          <div>{{ scope.opt.label }}</div>
+        </div>
+      </template>
 
-                <template v-slot:option="scope">
-                    <q-item clickable="row items-center" v-bind="scope.itemProps">
-                    <q-item-section avatar>
-                        <q-icon class="q-mr-md" color="grey-9" size="1.5rem" :name="scope.opt.icon" />
-                    </q-item-section>
-                    <q-item-section>
-                        <q-item-label>{{ scope.opt.label }}</q-item-label>
-                    </q-item-section>
-                    </q-item>
-                </template>
-                </q-select>
-        </q-form>
+      <template v-slot:option="scope">
+        <q-item clickable="row items-center" v-bind="scope.itemProps">
+          <q-item-section avatar>
+            <q-icon class="q-mr-md" color="grey-9" size="1.5rem" :name="scope.opt.icon" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ scope.opt.label }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </template>
+    </q-select>
+  </q-form>
 </template>
 
 <script setup lang="ts">
@@ -88,10 +84,15 @@ import { ref } from 'vue'
 import { QForm, QInput } from 'quasar'
 import { isEmail, required, minLength, maxLength } from '@/utils/form-validation'
 import { mdiEmail } from '@quasar/extras/mdi-v7'
-import { mdiAccount, mdiShieldAccountOutline, mdiLock, mdiEyeOff, mdiEye } from '@quasar/extras/mdi-v7'
-import  type { UserUpdate } from '@/models/Auth'
+import {
+  mdiAccount,
+  mdiShieldAccountOutline,
+  mdiLock,
+  mdiEyeOff,
+  mdiEye
+} from '@quasar/extras/mdi-v7'
+import type { UserUpdate } from '@/models/Auth'
 import { useI18n } from 'vue-i18n'
-import LanguageSelect from '@/components/LanguageSelect.vue'
 
 const { t } = useI18n()
 
@@ -106,10 +107,9 @@ const roles = [
 
 const isPwd = ref(true)
 
-
 const form = ref<QForm>()
 async function validate() {
-    return await form.value?.validate()
+  return await form.value?.validate()
 }
 
 defineExpose({ validate })
@@ -117,10 +117,10 @@ defineExpose({ validate })
 
 <style scoped>
 .gap {
-gap: 0.5rem;
+  gap: 0.5rem;
 }
 
 .gap-row {
-gap: 2rem;
+  gap: 2rem;
 }
 </style>
--- frontend/src/views/UserListView.vue
+++ frontend/src/views/UserListView.vue
@@ -1,64 +1,64 @@
 <template>
-    <PageLayout :title="t('users.create_users')">
-        <template #actions>
-            <q-btn
-                class="shadow"
-                color="primary"
-                :icon="mdiPlus"
-                :label="t('users.create_user')"
-                to="/users/create"
-                unelevated
-                no-caps
-                size="15px"
-            />
-        </template>
-        <template #default>
-        <div class="shadow">
-            <q-table
-            :rows="users"
-            :columns="columns"
-            row-key="id"
-            flat
-            :loading="loadingUsers"
-            :rows-per-page-options="[10, 20, 30]"
-            :no-data-label="t('vote.data.user_data_label')"
-            :loading-label="t('vote.data.user_loading_label')"
-            :rows-per-page-label="t('vote.data.user_rows_label')"
-            >
+  <PageLayout :title="t('users.create_users')">
+    <template #actions>
+      <q-btn
+        class="shadow"
+        color="primary"
+        :icon="mdiPlus"
+        :label="t('users.create_user')"
+        to="/users/create"
+        unelevated
+        no-caps
+        size="15px"
+      />
+    </template>
+    <template #default>
+      <div class="shadow">
+        <q-table
+          :rows="users"
+          :columns="columns"
+          row-key="id"
+          flat
+          :loading="loadingUsers"
+          :rows-per-page-options="[10, 20, 30]"
+          :no-data-label="t('vote.data.user_data_label')"
+          :loading-label="t('vote.data.user_loading_label')"
+          :rows-per-page-label="t('vote.data.user_rows_label')"
+        >
+          <template #body-cell-actions="propsActions">
+            <q-td auto-width :props="propsActions">
+              <!-- Edit button -->
+              <q-btn
+                :icon="mdiPencil"
+                color="gray-btn"
+                flat
+                round
+                :to="`/users/${propsActions.row.id}/edit`"
+              >
+                <q-tooltip content-style="font-size: 11px" :offset="[0, 4]">
+                  {{ t('users.edit_user') }}
+                </q-tooltip>
+              </q-btn>
 
-            <template #body-cell-actions="propsActions">
-                <q-td auto-width :props="propsActions">
-
-                <!-- Edit button -->
-                <q-btn
-                    :icon="mdiPencil"
-                    color="gray-btn"
-                    flat
-                    round
-                    :to="`/users/${propsActions.row.id}/edit`"
-                >
-                    <q-tooltip content-style="font-size: 11px" :offset="[0, 4]"> {{ t('users.edit_user') }} </q-tooltip>
-                </q-btn>
-
-                <!--Dropdown -->
-                <q-btn :icon="mdiDotsVertical" color="gray-btn" flat round>
-                    <q-menu anchor="bottom right" self="top right">
-                    <q-list class="text-grey-10">
-                        <q-item v-close-popup clickable @click="deleteUser(propsActions.row.id)">
-                        <div class="row items-center q-gutter-sm">
-                            <q-icon color="grey-8" size="24px" :name="mdiTrashCan" />
-                            <div>{{ t('questions.delete') }}</div>
-                        </div>
-                        </q-item>
-                    </q-list>
-                    </q-menu>
-                </q-btn>
-                </q-td>
-            </template>
-            </q-table>
-        </div>
-        </template>
-    </PageLayout>
+              <!--Dropdown -->
+              <q-btn :icon="mdiDotsVertical" color="gray-btn" flat round>
+                <q-menu anchor="bottom right" self="top right">
+                  <q-list class="text-grey-10">
+                    <q-item v-close-popup clickable @click="deleteUser(propsActions.row.id)">
+                      <div class="row items-center q-gutter-sm">
+                        <q-icon color="grey-8" size="24px" :name="mdiTrashCan" />
+                        <div>{{ t('questions.delete') }}</div>
+                      </div>
+                    </q-item>
+                  </q-list>
+                </q-menu>
+              </q-btn>
+            </q-td>
+          </template>
+        </q-table>
+      </div>
+    </template>
+  </PageLayout>
 </template>
 
 <script setup lang="ts">
@@ -66,12 +66,7 @@ import PageLayout from '@/layouts/PageLayout.vue'
 import { ref } from 'vue'
 import { toast } from 'vue3-toastify'
 import type { QTableProps } from 'quasar'
-import {
-    mdiDotsVertical,
-    mdiPencil,
-    mdiTrashCan,
-    mdiPlus
-} from '@quasar/extras/mdi-v7'
+import { mdiDotsVertical, mdiPencil, mdiTrashCan, mdiPlus } from '@quasar/extras/mdi-v7'
 import type { User } from '@/models/User'
 import UserService from '@/services/UserService'
 import { useAuthStore } from '@/stores/auth-store'
@@ -87,81 +82,83 @@ const router = useRouter()
 const authStore = useAuthStore()
 
 function verifyRole() {
-    if (authStore.role !== 'admin') {
-        toast.error(t('toast.unauthorized'))
-        router.push('/')
-    }
+  if (authStore.role !== 'admin') {
+    toast.error(t('toast.unauthorized'))
+    router.push('/')
+  }
 }
 verifyRole()
 
 async function getAllUsers() {
-    try {
-        loadingUsers.value = true
-        users.value = await UserService.getUsers()
-    } catch (error) {
-        console.error(error)
-        toast.error(t('toast.users.get_error'))
-    } finally {
-        loadingUsers.value = false
-    }
+  try {
+    loadingUsers.value = true
+    users.value = await UserService.getUsers()
+  } catch (error) {
+    console.error(error)
+    toast.error(t('toast.users.get_error'))
+  } finally {
+    loadingUsers.value = false
+  }
 }
 getAllUsers()
 
 async function deleteUser(id: string) {
-    try {
-        await UserService.deleteUser(id)
-        toast.success(t('toast.users.delete'))
-        getAllUsers()
-    } catch (error) {
-        console.error(error)
-        toast.error(t('toast.users.delete_error'))
-    }
+  try {
+    await UserService.deleteUser(id)
+    toast.success(t('toast.users.delete'))
+    getAllUsers()
+  } catch (error) {
+    console.error(error)
+    toast.error(t('toast.users.delete_error'))
+  }
 }
 
 const columns: QTableProps['columns'] = [
-    {
-        name: 'id',
-        label: 'Id',
-        field: 'id',
-        align: 'left',
-        sortable: true
+  {
+    name: 'id',
+    label: 'Id',
+    field: 'id',
+    align: 'left',
+    sortable: true
+  },
+  {
+    name: 'email',
+    label: 'Email',
+    field: 'email',
+    align: 'left',
+    sortable: true
+  },
+  {
+    name: 'first_name',
+    label: t('users.first_name'),
+    field: 'first_name',
+    align: 'left',
+    sortable: true
+  },
+  {
+    name: 'last_name',
+    label: t('users.last_name'),
+    field: 'last_name',
+    align: 'left',
+    sortable: true
+  },
+  {
+    name: 'role',
+    label: t('users.roles.role'),
+    field: 'role',
+    align: 'left',
+    format(val, row) {
+      return row.role === 'admin' ? t('users.roles.admin') : t('users.roles.user')
     },
-    {
-        name: 'email',
-        label: 'Email',
-        field: 'email',
-        align: 'left',
-        sortable: true
-    },
-    {
-        name: 'first_name',
-        label: t('users.first_name'),
-        field: 'first_name',
-        align: 'left',
-        sortable: true
-    },
-    {
-        name: 'last_name',
-        label: t('users.last_name'),
-        field: 'last_name',
-        align: 'left',
-        sortable: true
-    },
-    {
-        name: 'role',
-        label: t('users.roles.role'),
-        field: 'role',
-        align: 'left',
-        sortable: true
-    },
-    {
-        name: 'actions',
-        label: t('columns.actions'),
-        align: 'center',
-        field: ''
-    }
+    sortable: true
+  },
+  {
+    name: 'actions',
+    label: t('columns.actions'),
+    align: 'center',
+    field: ''
+  }
 ]
 </script>
 
 <style scoped></style>
-  
